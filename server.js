750cd187d1634e8fdb97dae8b17faf209747c35f
Victor Lia Fook <victorliafook@gmail.com>
2017-04-17 06:08:20 +0000

    functionality and index ok

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var multer = require('multer');
 
-var upload = multer({ dest: 'uploads/' });
+var upload = multer({
+    dest: 'uploads/'
+});
 
 var app = express();
+app.use(express.static('public'));
+
+app.post('/get-file-size', upload.single('file'), function(req, res, next) {
+    console.log(req);
+    // req.file is the `file` file 
+    res.json({size: req.file.size});
+});
 
 app.listen(process.env.PORT || 8081);
